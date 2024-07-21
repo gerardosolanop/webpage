41cb95df76dec67d3f19e53a844257e131106afa
gerardosolanop <gerardosolanop@gmail.com>
2024-07-21 14:46:12 -0400

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,17 @@
+<!-- nav.php -->
+<nav class="menu">
+    <ul class="nav">
+        <li class="limenu"><a class="aoption" href="index.php">Home</a></li>
+        <li class="limenu">
+            <a class="aoption" href="resume.php">Resume</a>
+            <ul class="subnav">
+                <li class="lisubmenu"><a class="suba" href="resume.php#experience">Experience</a></li>
+                <li class="lisubmenu"><a class="suba" href="resume.php#skills">Skills</a></li>
+                <li class="lisubmenu"><a class="suba" href="resume.php#tools">Tools</a></li>
+                <li class="lisubmenu"><a class="suba" href="resume.php#education">Education</a></li>
+            </ul>
+        </li>
+        <li class="limenu"><a class="aoption" href="aboutus.php">About Us</a></li>
+        <li class="limenu"><a class="aoption" href="portfolio.php">Portfolio</a></li>
+    </ul>
+</nav>
